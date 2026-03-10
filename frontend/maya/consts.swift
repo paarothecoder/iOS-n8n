748ed46190b05a7a411f277c801920d2bac1ae00
paaro <pratyushraj0003@gmail.com>
2026-03-10 13:22:58 +0530

    Modify link and kajo constants in const struct
    Updated constants for link and  URLs.

--- frontend/maya/consts.swift
+++ frontend/maya/consts.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct const {
-    static let link = "10.168.8.119"
-    static let kajo = "https://imgs.search.brave.com/3oqy3KWmfV6yLDj0VcbmT_YPR2ybvKjNsWnmhY8ZnHk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDM5OTE5/NTUuanBn"
+    static let link = ""
+    static let kajo = "https://imgs.search.brave.com/_Oxk-HHpry4ynvOKBPdKAs-jQSay9J2NctWp9QJc0UU/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvODI5/NDYwNi9wZXhlbHMt/cGhvdG8tODI5NDYw/Ni5qcGVnP2F1dG89/Y29tcHJlc3MmY3M9/dGlueXNyZ2ImZHBy/PTEmdz01MDA"
     static let url = "http://\(link):8000/"
 }
